a5570838067da794f3129ae66afd1c67825b28bc
lusia <panfluba@gmail.com>
2023-11-08 02:57:33 +0300

    feat: make task12

--- index.php
+++ index.php
@@ -1,1 +1,12 @@
-<!--initial file -->
+<?php
+function automorphic($int) : bool{
+    return str_ends_with(strval($int*$int), $int);
+}
+
+echo automorphic(1). " - 1\n";
+echo automorphic(3) . " - 3\n";
+// 3^2 = 9
+echo automorphic(6). " - 6\n";
+// 6^2 = 36 (ends with 6)
+echo automorphic(95). " - 95\n";
+// 95^2 = 9025 (does not end with 95)
