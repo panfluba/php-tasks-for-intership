ead0d132b5394277a9eb60d62bb462fc42fdd7fc
lusia <panfluba@gmail.com>
2023-11-08 00:54:37 +0300

    feat: make task10

--- index.php
+++ index.php
@@ -1,1 +1,12 @@
-<!--initial file -->
+<?php
+function reverse($str){
+    if(strlen($str) == 0 || strlen($str) == 1){
+        return $str;
+    }
+    else return reverse(substr($str, 1)) . $str[0];
+}
+
+echo reverse("hello") . "\n";
+echo reverse("world"). "\n";
+echo reverse("a"). "\n";
+echo reverse(""). "\n";
