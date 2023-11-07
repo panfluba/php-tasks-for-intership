3a634d7bf4a6b7af79c72e12eae86e336895caf2
lusia <panfluba@gmail.com>
2023-11-07 18:09:28 +0300

    feat: make task8

--- index.php
+++ index.php
@@ -1,1 +1,14 @@
-<!--initial file -->
+<?php
+function solutions($a, $b, $c){
+    if(($b**2 - (4 * $a * $c)) > 0){
+        return 2;
+    }
+    elseif(($b**2 - (4 * $a * $c)) == 0){
+        return 1;
+    }
+    else return 0;
+}
+
+echo solutions(1, 0, -1);
+echo solutions(1, 0, 0);
+echo solutions(1, 0, 1);
