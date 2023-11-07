52dca65ab1de1cfab833efbc50390bfcb58e2084
lusia <panfluba@gmail.com>
2023-11-08 02:34:05 +0300

    feat: make task11

--- index.php
+++ index.php
@@ -1,1 +1,19 @@
-<!--initial file -->
+<?php
+function removeSmallest($arr){
+
+    if (!empty($arr)) {
+    $smallest = min($arr);
+        foreach ($arr as $key => $value){
+            if($value == $smallest){
+                unset($arr[$key]);
+                return implode(',', $arr) . "\n";
+            }
+        }
+    }
+    return "Array is empty";
+}
+
+echo removeSmallest([1, 2, 3, 4, 5]);
+echo removeSmallest([5, 3, 2, 1, 4]);
+echo removeSmallest([2, 2, 1, 2, 1]);
+echo removeSmallest([]);
