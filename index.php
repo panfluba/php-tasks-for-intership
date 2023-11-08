65e1c6564ae86d4c64b7db3ef38bc3337c69b41b
lusia <panfluba@gmail.com>
2023-11-08 13:47:28 +0300

    feat: make task14

--- index.php
+++ index.php
@@ -1,1 +1,18 @@
-<!--initial file -->
+<?php
+function hurdleJump($arr, $height){
+
+    if(empty($arr)){
+        return "true";
+    }
+
+    if(max($arr) > $height){
+        return "false";
+    }
+    else return 'true';
+}
+
+echo hurdleJump([1, 2, 3, 4, 5], 5); // ➞ true
+echo hurdleJump([5, 5, 3, 4, 5], 3); // ➞ false
+echo hurdleJump([5, 4, 5, 6], 10); // ➞ true
+echo hurdleJump([1, 2, 1], 1); // ➞ false
+echo hurdleJump([], 0); // ➞ true
