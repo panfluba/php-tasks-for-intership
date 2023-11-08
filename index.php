19e8690b8ee7e47b868e122e83e7f1a9983d9987
lusia <panfluba@gmail.com>
2023-11-08 14:51:21 +0300

    feat: make task21

--- index.php
+++ index.php
@@ -1,1 +1,8 @@
-<!--initial file -->
+<?php
+function giveMeSomething($a){
+    return "something {$a}\n" ; // or concat - return "something " . $a;
+}
+
+echo giveMeSomething("is better than nothing");// ➞ "something is better than nothing"
+echo giveMeSomething("Bob Jane");// ➞ "something Bob Jane"
+echo giveMeSomething("something");// ➞ "something something"
