ab79516e0cf09aa7a099941102529c62a11ed61f
lusia <panfluba@gmail.com>
2023-11-08 13:58:32 +0300

    feat: make task15

--- index.php
+++ index.php
@@ -1,1 +1,14 @@
-<!--initial file -->
+<?php
+function getAbsSum($arr){
+    foreach ($arr as $key => $value) {
+        if($value < 0){
+            $arr[$key] = $value * (-1);
+        }
+    }
+    var_dump(array_sum($arr));
+}
+
+getAbsSum([2, -1, 4, 8, 10]); // ➞ 25
+getAbsSum([-3, -4, -10, -2, -3]); // ➞ 22
+getAbsSum([2, 4, 6, 8, 10]); // ➞ 30
+getAbsSum([-1]); // ➞ 1
