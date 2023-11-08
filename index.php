7c7982fe88e9e33eb4cc2e5fbdd7621e49151ba7
lusia <panfluba@gmail.com>
2023-11-08 14:31:32 +0300

    feat: make task18

--- index.php
+++ index.php
@@ -1,1 +1,28 @@
-<!--initial file -->
+<?php
+function bbqSkewers($arr): void{
+    $vegan = 0;
+    $nonVegan = 0;
+    foreach ($arr as $value){
+        if(str_contains($value, "x")){
+            $nonVegan++;
+        }
+        else $vegan++;
+    }
+    echo "[{$vegan}, {$nonVegan}]\n";
+}
+
+bbqSkewers([
+        "--oooo-ooo--",
+        "--xx--x--xx--",
+        "--o---o--oo--",
+        "--xx--x--ox--",
+        "--xx--x--ox--"
+    ]); // ➞ [2, 3]
+
+bbqSkewers([
+    "--oooo-ooo--",
+    "--xxxxxxxx--",
+    "--o---",
+    "-o-----o---x--",
+    "--o---o-----"
+]); // ➞ [3, 2]
