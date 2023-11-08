a62d2d17dfe68afcb6b771c14b8013b54cb324df
lusia <panfluba@gmail.com>
2023-11-08 15:01:32 +0300

    feat: make task23

--- index.php
+++ index.php
@@ -1,1 +1,13 @@
-<!--initial file -->
+<?php
+function hasSpaces($str) : string{
+    if(str_contains($str, " ")){
+        return "true\n";
+    }
+    else return "false\n";
+}
+
+echo hasSpaces("hello");// ➞ false
+echo hasSpaces("hello, world");// ➞ true
+echo hasSpaces(" ");// ➞ true
+echo hasSpaces("");// ➞ false
+echo hasSpaces(",./!@#");// ➞ false
