06449aa0052351fef7f6f4dc9897bbcd8c967c84
lusia <panfluba@gmail.com>
2023-11-08 14:05:09 +0300

    feat: make task16

--- index.php
+++ index.php
@@ -1,1 +1,9 @@
-<!--initial file -->
+<?php
+function toArray($str){
+    var_dump(explode(", ", $str));
+}
+
+toArray("арбуз, малина, апельсин");//➞ ["арбуз", "малина", "апельсин"]
+toArray("x1, x2, x3, x4, x5");//➞ ["x1", "x2", "x3", "x4", "x5"]
+toArray("a, b, c, d");//➞ ["a", "b", "c", "d"]
+toArray("");//➞ []
