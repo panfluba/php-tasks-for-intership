4ea4dda78bff63dcf7241217e806691dcdcd15de
lusia <panfluba@gmail.com>
2023-11-08 14:55:32 +0300

    feat: make task22

--- index.php
+++ index.php
@@ -1,1 +1,11 @@
-<!--initial file -->
+<?php
+function lessThanOrEqualToZero($num){
+    if($num <= 0){
+        return "true\n";
+    }
+    else return "false\n";
+}
+
+echo lessThanOrEqualToZero(5);// ➞ false
+echo lessThanOrEqualToZero(0);// ➞ true
+echo lessThanOrEqualToZero(-2);// ➞ true
