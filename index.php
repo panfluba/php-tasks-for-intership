9699aab43ab7bcdcc6811a9cb92cd20e867832a7
lusia <panfluba@gmail.com>
2023-11-08 14:38:46 +0300

    feat: make task19

--- index.php
+++ index.php
@@ -1,1 +1,9 @@
-<!--initial file -->
+<?php
+function addition($num1, $num2){
+    return $num1 + $num2 . "\n";
+}
+
+
+echo addition(3, 2);// ➞ 5
+echo addition(-3, -6);// ➞ -9
+echo addition(7, 3);// ➞ 10
