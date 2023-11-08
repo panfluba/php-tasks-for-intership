04578de6bd6db55199332e095751a700b98b49f2
lusia <panfluba@gmail.com>
2023-11-08 14:18:47 +0300

    feat: make task17

--- index.php
+++ index.php
@@ -1,1 +1,14 @@
-<!--initial file -->
+<?php
+function alphabetSoup($str){
+    $arr = str_split($str);
+    sort($arr);
+    foreach ($arr as $value){
+        echo $value;
+    }
+}
+
+alphabetSoup("hello");// ➞ "ehllo"
+alphabetSoup("edabit");// ➞ "abdeit"
+alphabetSoup("hacker");// ➞ "acehkr"
+alphabetSoup("geek");// ➞ "eegk"
+alphabetSoup("javascript");// ➞ "aacijprstv"
