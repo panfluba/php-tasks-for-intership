806e220e311d5573c2b4026986839eeec71117cd
lusia <panfluba@gmail.com>
2023-11-08 13:37:53 +0300

    feat: make task13

--- index.php
+++ index.php
@@ -1,1 +1,25 @@
-<!--initial file -->
+<?php
+function calculateScores($str){
+    $countA = 0;
+    $countB = 0;
+    $countC = 0;
+    $arr = str_split($str);
+    foreach($arr as $value){
+        if($value == "A"){
+            $countA++;
+        }
+        elseif($value == "B"){
+            $countB++;
+        }
+        elseif($value == "C"){
+            $countC++;
+        }
+    }
+    var_dump($countA . $countB . $countC);
+}
+calculateScores("A");
+//[1, 0, 0]
+calculateScores("ABC");
+//➞ [1, 1, 1]
+calculateScores("ABCBACC");
+//➞ [2, 2, 3]
