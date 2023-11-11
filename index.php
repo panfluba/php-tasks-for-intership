c16485e7f3dfc558a22080652f753f858b7a1ca5
lusia <panfluba@gmail.com>
2023-11-11 04:34:19 +0300

    feat: add solution (ternary expression)

--- index.php
+++ index.php
@@ -9,6 +9,16 @@ function solutions($a, $b, $c){
     else return 0;
 }
 
+function solutions1($a, $b, $c){
+// через тернарку (для большей читабельности кода вынесла уравнение в отдельную переменную)
+    $exp = $b**2 - (4 * $a * $c);
+    return $exp > 0 ? 2 : ($exp == 0 ? 1 : 0);
+}
+
 echo solutions(1, 0, -1);
 echo solutions(1, 0, 0);
 echo solutions(1, 0, 1);
+
+echo solutions1(1, 0, -1);
+echo solutions1(1, 0, 0);
+echo solutions1(1, 0, 1);
