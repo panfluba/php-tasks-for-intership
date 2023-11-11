9c00d12e70ba418bd88db6f3e339fe8a8ddb202d
lusia <panfluba@gmail.com>
2023-11-11 04:52:01 +0300

    feat: add solution to task12

--- index.php
+++ index.php
@@ -1,6 +1,10 @@
 <?php
 function automorphic($int) : bool{
-    return str_ends_with(strval($int*$int), $int);
+    return str_ends_with($int*$int, $int);
+}
+
+function automorphic2($int) : bool{
+    return $int * $int % 10 * strlen($int) == $int;
 }
 
 echo automorphic(1). " - 1\n";
@@ -10,3 +14,8 @@ echo automorphic(6). " - 6\n";
 // 6^2 = 36 (ends with 6)
 echo automorphic(95). " - 95\n";
 // 95^2 = 9025 (does not end with 95)
+
+echo automorphic2(1). " - 1\n";
+echo automorphic2(3) . " - 3\n";
+echo automorphic2(6). " - 6\n";
+echo automorphic2(95). " - 95\n";
