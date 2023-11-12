70f6cf2f96a4694d835bbb6c4b570fa8aefbafff
lusia <panfluba@gmail.com>
2023-11-13 02:30:33 +0300

    refactor: change the return value

--- index.php
+++ index.php
@@ -1,18 +1,17 @@
 <?php
 function hurdleJump($arr, $height){
-
     if(empty($arr)){
-        return "true";
+        return true;
     }
 
     if(max($arr) > $height){
-        return "false";
+        return false;
     }
-    else return 'true';
+    else return true;
 }
 
-echo hurdleJump([1, 2, 3, 4, 5], 5); // ➞ true
-echo hurdleJump([5, 5, 3, 4, 5], 3); // ➞ false
-echo hurdleJump([5, 4, 5, 6], 10); // ➞ true
-echo hurdleJump([1, 2, 1], 1); // ➞ false
-echo hurdleJump([], 0); // ➞ true
+hurdleJump([1, 2, 3, 4, 5], 5); // ➞ true
+hurdleJump([5, 5, 3, 4, 5], 3); // ➞ false
+hurdleJump([5, 4, 5, 6], 10); // ➞ true
+hurdleJump([1, 2, 1], 1); // ➞ false
+hurdleJump([], 0); // ➞ true
